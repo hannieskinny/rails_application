f78ba867e3d470a9ac049cf53f486348f7955a2f
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-06 10:29:57 -0500

    Create project rails

---
+++ rails_exam/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsExam::Application.config.secret_key_base = '30891516123ab991e0f447c63f4f4b5f57b1a03202ed63f2740c502cc2d44adbe75d4e60d0adff31c61dad397203e8156bbcfe6cf36128a1005501b7bc1724b7'
