5814f4860878d9ff96a3f4bad626d8e8406d6e1f
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-06 13:04:58 -0500

    Add method and view show

--- rails_exam/app/controllers/cars_controller.rb
+++ rails_exam/app/controllers/cars_controller.rb
@@ -8,13 +8,17 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(car_params)
-    if @project.save
+    if @car.save
       flash[:notice] = "Car has been created"
       redirect_to @car
     else
     end
   end
-  
+ 
+  def show
+    @car = Car.find(params[:id])
+  end
+
 private
   def car_params
     params.require(:car).permit(:brand, :year)
