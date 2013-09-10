c7ae7c311e8abcc2c808ade3cd39d12437c6ac4f
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-10 18:03:05 -0500

    delete contex

--- rails_exam/spec/features/create_comments_spec.rb
+++ rails_exam/spec/features/create_comments_spec.rb
@@ -1,25 +1,21 @@
 require 'spec_helper'
 
 feature "Creating comments" do
-  context 'Create car' do
-    let!(:car){ FactoryGirl.create(:car) }
-  end
+  let!(:car){ FactoryGirl.create(:car) }
   before do
     visit car_path(car)
-    #click_link car.brand
   end
 
   scenario "Creating a comment" do
     fill_in "comment_text", :with => "Added a comment!"
     click_button "create-comment"
     page.should have_content("Comment has been created.")
-      within("#comments") do
-        page.should have_content("Added a comment!")
-      end
+    within("#comments") do
+      page.should have_content("Added a comment!")
+    end
   end
   scenario "Creating an invalid comment" do
-      click_button "create-comment"
-      page.should have_content("Comment has not been created.")
-      page.should have_content("Text can't be blank")
+    click_button "create-comment"
+    page.should have_content("Comment has not been created.")
   end 
 end
