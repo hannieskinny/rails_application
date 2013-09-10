8b63867bb8b8644001b28ad0cb71e3e23b992a31
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-10 11:57:48 -0500

    Add name id of field

--- rails_exam/spec/features/attach_many_files_spec.rb
+++ rails_exam/spec/features/attach_many_files_spec.rb
@@ -4,9 +4,9 @@ feature 'Attachment many files' do
  visit new_car_path
     fill_in "car_brand", with: "tsuru"
     fill_in "car_year", with: "1990"
-    attach_file "car_asset", Rails.root.join("spec/fixtures/car.JPG")
-    attach_file "car_asset2", Rails.root.join("spec/fixtures/car2.jpg")
-    attach_file "car_asset3", Rails.root.join("spec/fixtures/car3.jpg")
+    attach_file "car_assets_attributes_0_asset", Rails.root.join("spec/fixtures/car.JPG")
+    attach_file "car_assets_attributes_1_asset", Rails.root.join("spec/fixtures/car2.jpg")
+    attach_file "car_assets_attributes_2_asset", Rails.root.join("spec/fixtures/car3.jpg")
     click_button "create-car"
     expect(page).to have_content("Car has been created")
     within(".assets") do
