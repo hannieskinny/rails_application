df01f22be1d2adef5c3f8a43311cd6c746736b17
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-10 14:40:22 -0500

    Add method for create comments

--- rails_exam/app/controllers/comments_controller.rb
+++ rails_exam/app/controllers/comments_controller.rb
@@ -1,3 +1,23 @@
 class CommentsController < ApplicationController
+before_filter :find_car
+   
+  def create
+    @comment = @car.comments.build(comment_params)
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@car]
+    else
+      flash[:alert] = "Comment has not been created."
+      render :template => "cars/show"
+    end
+  end 
 
+  private
+  def find_car
+    @car = Car.find(params[:car_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
