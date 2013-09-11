494f14d4a0096627ea7a4ccef9f8dfbceae7ff8d
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-11 11:20:47 -0500

    Add helper for state

---
+++ rails_exam/app/helpers/cars_helper.rb
@@ -0,0 +1,14 @@
+module CarsHelper
+  def state_for(comment)
+    content_tag(:div, :class => "states") do
+      if comment.state
+        previous_state = comment.previous_state
+        if previous_state && comment.state != previous_state
+          "#{render previous_state} &rarr; #{render comment.state}"
+        else
+          render(comment.state)
+        end
+      end 
+    end
+  end 
+end
