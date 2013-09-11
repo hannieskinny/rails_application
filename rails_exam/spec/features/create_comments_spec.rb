49fc8d86125ef1f4954d5726d7a308eae63697c3
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-11 14:07:11 -0500

    changes all to context

--- rails_exam/spec/features/create_comments_spec.rb
+++ rails_exam/spec/features/create_comments_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 feature "Creating comments" do
   context 'Create car' do
     let!(:car){ FactoryGirl.create(:car) }
-  end
-  before do
-    visit car_path(car)
-    #click_link car.brand
-  end
 
-  scenario "Creating a comment" do
-    fill_in "comment_text", :with => "Added a comment!"
-    click_button "create-comment"
-    page.should have_content("Comment has been created.")
-      within("#comments") do
-        page.should have_content("Added a comment!")
-      end
-  end
-  scenario "Creating an invalid comment" do
+    before do
+      visit car_path(car)
+      #click_link car.brand
+    end
+
+    scenario "Creating a comment" do
+      fill_in "comment_text", :with => "Added a comment!"
       click_button "create-comment"
-      page.should have_content("Comment has not been created.")
-      page.should have_content("Text can't be blank")
-  end 
+      page.should have_content("Comment has been created.")
+        within("#comments") do
+          page.should have_content("Added a comment!")
+        end
+    end
+    scenario "Creating an invalid comment" do
+        click_button "create-comment"
+        page.should have_content("Comment has not been created.")
+    end 
+  end
 end
