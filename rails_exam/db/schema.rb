4e8553f0ad2894307cad1751c97fedc99601f52e
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-11 11:21:48 -0500

    Add migration

--- rails_exam/db/schema.rb
+++ rails_exam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910214831) do
+ActiveRecord::Schema.define(version: 20130910233832) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20130910214831) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "state_id"
+    t.integer  "previous_state_id"
   end
 
   create_table "states", force: true do |t|
