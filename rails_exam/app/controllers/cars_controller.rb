1be8281c507b6908131458c2c5bc018b815cb3aa
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-12 14:47:11 -0500

    Add method authorize

--- rails_exam/app/controllers/cars_controller.rb
+++ rails_exam/app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
-  before_action :require_signin!, except: [:show, :index]
-
+  before_action :authorize_admin!, except: [:index, :show]
+  #before_action :set_car, only: [:show, :edit, :update, :destroy]
   def index
     @car = Car.all
   end
@@ -55,4 +55,12 @@ private
   def car_params
     params.require(:car).permit(:brand, :year, :tag_names, :user_id, assets_attributes: [:asset])
   end
+
+  def authorize_admin!
+    require_signin!
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+    end 
+  end
+
 end
