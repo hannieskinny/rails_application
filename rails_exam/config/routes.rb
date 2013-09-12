ec46e718b80f685aa0d8a50627f4845043dd45d8
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-11 19:07:36 -0500

    Add member search for method

--- rails_exam/config/routes.rb
+++ rails_exam/config/routes.rb
@@ -1,5 +1,13 @@
 RailsExam::Application.routes.draw do
   resources :cars do
     resources :comments
+    resources :tags do
+      member do
+        delete :remove
+      end
+    end
+    collection do
+      get :search
+    end
   end
 end
