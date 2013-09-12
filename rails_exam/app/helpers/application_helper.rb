900cd085977b3644e0d6ab45167f8987e158e4d1
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-12 14:48:21 -0500

    Add method current_user

--- rails_exam/app/helpers/application_helper.rb
+++ rails_exam/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+  end
+
+  def current_user
+     User.find_by_id!(session[:user_id]) if session[:user_id] != nil
+  end
 end
+
