2faa9d35e92cbf536a9dd3a7d8d3f0655ceb43b4
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-13 16:07:20 -0500

    Changes name method

--- rails_exam/app/mailers/notifier.rb
+++ rails_exam/app/mailers/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   from_address = ActionMailer::Base.smtp_settings[:user_name]
   default from: "Car App <#{from_address}>"
-  def user_created(user)
+  def send_email_to(user)
     @user = user
     subject = "Welcome #{user.name} !"
     mail(:to => user.email, :subject => subject)
--- rails_exam/config/initializers/mail.rb
+++ rails_exam/config/initializers/mail.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
 :user_name => 'alejandra.gutierrez@tangosource.com',
-:password => 'ladyflakita',
+:password => 'password',
 :address => 'smtp.gmail.com',
 :port => 587,
 :enable_starttls_auto => true
