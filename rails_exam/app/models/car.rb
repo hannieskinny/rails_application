4b31431e791149c1ba2c12068a0add122362ff6b
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-13 12:22:53 -0500

    Solve problems with branch

--- rails_exam/app/models/car.rb
+++ rails_exam/app/models/car.rb
@@ -1,27 +1,12 @@
 class Car < ActiveRecord::Base
-<<<<<<< HEAD
-  def searcher
-    label :tag, :from => :tags, :field => :name
-  end
-  
-=======
   searcher do
     label :tag, :from => :tags, :field => :name
   end
-
->>>>>>> add-authentication-in-project
   validates :brand, :presence => true
   has_many :assets
   accepts_nested_attributes_for :assets
   has_many :comments
-<<<<<<< HEAD
-  mount_uploader :asset, AssetUploader
   belongs_to :user
-
-=======
-  belongs_to :user
-  
->>>>>>> add-authentication-in-project
   belongs_to :state
   attr_accessor :tag_names
   has_and_belongs_to_many :tags
