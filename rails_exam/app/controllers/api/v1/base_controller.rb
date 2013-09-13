ce86a338c51fb546394552eb7972c810a26dccab
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-13 16:15:39 -0500

    Create api controllers

---
+++ rails_exam/app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::BaseController < ActionController::Base
+  before_filter :authenticate_user
+  before_filter :authorize_admin!, :except => [:index, :show]
+  respond_to :json, :xml
+
+  private
+
+  def authenticate_user
+    @current_user = User.find_by_id!(session[:user_id]) if session[:user_id] != nil
+    unless @current_user
+      respond_with({:error => "Token is invalid." })
+    end
+  end
+  
+  def current_user
+    @current_user
+  end
+
+  def authorize_admin!
+    if !@current_user.admin?
+      error = { :error => "You must be an admin to do that." }
+      warden.custom_failure!
+      render params[:format].to_sym => error, :status => 401
+    end
+  end
+
+end
---
+++ rails_exam/app/controllers/api/v1/cars_controller.rb
@@ -0,0 +1,41 @@
+class Api::V1::CarsController < Api::V1::BaseController
+  before_filter :find_car, :only => [:show, :update, :destroy]
+  
+  def index
+    #respond_with(Car.all)
+    respond_with(Car.all.where("user_id = ?", current_user))
+  end
+
+  def create
+    car = car.new(params[:car])
+    if car.save
+      respond_with(car, :location => api_v1_car_path(car))
+    else
+      respond_with(car)
+    end
+  end
+
+  def show
+    car = Car.find(params[:id])
+    respond_with(car, :methods => "last_ticket")
+  end
+
+  def update
+    @car.update_attributes(params[:car])
+    respond_with(@car)
+  end
+
+  def destroy
+    @car.destroy
+    respond_with(@car)
+  end
+
+  private
+    def find_car
+      @car = Car.for(current_user).find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      error = { :error => "The car you were looking for " +
+                          "could not be found."}
+      respond_with(error, :status => 404)
+    end
+end
