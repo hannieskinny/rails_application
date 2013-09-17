6f89ca71f7c79563d97e02318383d74bba658500
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 15:24:58 -0500

    Add method admin

--- rails_exam/app/controllers/users_controller.rb
+++ rails_exam/app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       flash[:notice] = "User saved correctly"
       redirect_to @user
@@ -22,5 +21,13 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  def authorize_admin!
+    require_signin!
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+    end 
+  end
+
 
 end
