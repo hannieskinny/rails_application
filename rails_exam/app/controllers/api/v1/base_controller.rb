d47f0d11af847a985eae6fff28b62875cad56c7d
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 15:03:11 -0500

    Add method authorize

--- rails_exam/app/controllers/api/v1/base_controller.rb
+++ rails_exam/app/controllers/api/v1/base_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::BaseController < ActionController::Base
   before_filter :authenticate_user
-  before_filter :authorize_admin!, :except => [:index, :show]
   respond_to :json, :xml
 
   private
 
   def authenticate_user
-    @current_user = User.find_by_id!(session[:user_id]) if session[:user_id] != nil
+    @current_user = User.find_by_authentication_token(params[:token])
     unless @current_user
       respond_with({:error => "Token is invalid." })
     end
@@ -14,14 +13,6 @@ class Api::V1::BaseController < ActionController::Base
   
   def current_user
     @current_user
-  end
-
-  def authorize_admin!
-    if !@current_user.admin?
-      error = { :error => "You must be an admin to do that." }
-      warden.custom_failure!
-      render params[:format].to_sym => error, :status => 401
-    end
   end
 
 end
