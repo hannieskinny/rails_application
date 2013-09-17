e4967250bb1b68e5020846ffd8f6f175a2643b0b
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 15:04:38 -0500

    Add migration token

--- rails_exam/db/schema.rb
+++ rails_exam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917155858) do
+ActiveRecord::Schema.define(version: 20130917174826) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20130917155858) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
