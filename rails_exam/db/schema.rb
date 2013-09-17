febb155ea355bc028d87798e4deaf258c5b09a36
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-17 10:51:13 -0500

    add migration

--- rails_exam/db/schema.rb
+++ rails_exam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130912173438) do
+ActiveRecord::Schema.define(version: 20130914000810) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
@@ -45,6 +45,22 @@ ActiveRecord::Schema.define(version: 20130912173438) do
     t.integer  "state_id"
     t.integer  "previous_state_id"
   end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "states", force: true do |t|
     t.string "name"
