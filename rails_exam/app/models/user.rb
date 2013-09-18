369e57d1e675429a48f9c9386e0bd91d72132595
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-18 15:18:41 -0500

    Add method find twitter auth

--- rails_exam/app/models/user.rb
+++ rails_exam/app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :omniauthable, :omniauth_providers => [:twitter]
   has_many :cars
   before_save :ensure_authentication_token
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                          provider:auth.provider,
+                          uid:auth.uid,
+                          email:auth.info.email,
+                          password:Devise.friendly_token[0,20]
+                        )
+    end
+    user
+  end
 end
