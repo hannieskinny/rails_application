4604394cf9b2ba2b27cb2424a5990fa6fc150dab
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-18 18:42:38 -0500

    Comment line and method token

--- rails_exam/app/controllers/api/v1/base_controller.rb
+++ rails_exam/app/controllers/api/v1/base_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::BaseController < ActionController::Base
-  before_filter :authenticate_user
+before_filter :authenticate_user!
   respond_to :json, :xml
 
   private
 
-  def authenticate_user
-    @current_user = User.find_by_authentication_token(params[:token])
-    unless @current_user
-      respond_with({:error => "Token is invalid." })
-    end
-  end
+  #def authenticate_user
+   # @current_user = User.find_by_authentication_token(params[:token])
+    #unless @current_user
+     # respond_with({:error => "Token is invalid." })
+    #end
+  #end
   
   def current_user
     @current_user
