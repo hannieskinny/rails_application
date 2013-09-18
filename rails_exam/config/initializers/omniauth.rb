66b166fd1f906a5142a2d80f2f01174fd9597f31
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-18 12:42:08 -0500

    Add key and code secret

--- rails_exam/config/initializers/omniauth.rb
+++ rails_exam/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "CONSUMER_KEY", "CONSUMER_SECRET"
+  provider :twitter, "f9dcLcRBthr5jybUNmAUA", "MQy8DudsX8Q7vOzKIhUFHoiYn0hm2eMeWNnLEIKUj4"
 end
