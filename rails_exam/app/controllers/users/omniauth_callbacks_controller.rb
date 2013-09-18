a1b81f7e984e62fa56fcf833ef03d85a5c584f95
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-18 17:08:17 -0500

    Changes params raw_info

--- rails_exam/app/controllers/users/omniauth_callbacks_controller.rb
+++ rails_exam/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def twitter
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"].except("extra"), current_user)
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "twitter") if is_navigational_format?
