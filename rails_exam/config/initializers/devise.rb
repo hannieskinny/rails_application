02c7d2e542d4cbcb3fa0c963d8c3bc5c78eadbe0
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-18 14:56:03 -0500

    No verificate certificate gem

--- rails_exam/config/initializers/devise.rb
+++ rails_exam/config/initializers/devise.rb
@@ -15,7 +15,8 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
   require "omniauth-twitter"
-  config.omniauth :twitter, "f9dcLcRBthr5jybUNmAUA", "MQy8DudsX8Q7vOzKIhUFHoiYn0hm2eMeWNnLEIKUj4"
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? 
+  config.omniauth :twitter, "f9dcLcRBthr5jybUNmAUA", "MQy8DudsX8Q7vOzKIhUFHoiYn0hm2eMeWNnLEIKUj4", :strategy_class => OmniAuth::Strategies::Twitter
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
