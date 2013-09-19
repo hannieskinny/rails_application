68a905c2a0d3bc761636f4f0053a9f569a6cad86
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-19 10:20:33 -0500

    Delete code unnecessary

--- rails_exam/app/controllers/users/omniauth_callbacks_controller.rb
+++ rails_exam/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  
   def twitter
-    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"].except("extra"), current_user)
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "twitter") if is_navigational_format?
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"])
+    flash[:notice] = "Signed in with Twitter successfully."
+    sign_in_and_redirect @user, :event => :authentication
   end
 
 end
