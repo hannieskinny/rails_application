51d378a31299fea92e3e8b494f083c479b12d7e5
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-09-19 18:04:37 -0500

    Add migration of delayed jon

--- rails_exam/db/schema.rb
+++ rails_exam/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918194255) do
+ActiveRecord::Schema.define(version: 20130919230246) do
 
   create_table "assets", force: true do |t|
     t.string   "asset"
